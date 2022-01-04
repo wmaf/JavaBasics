c6f8876e37659613c72c5de11b98467232d90934
wmaf <wmaf@outlook.com>
2022-01-04 12:52:05 -0500

    Corrected version of classwork.

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -1,8 +1,25 @@
 package com.careerdevs;
 
 public class Main {
-
+// 1/3 JavaBasics
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        String firstName = "Bill ";
+        String lastName = "Ford";
+        String fullName = firstName + lastName;
+
+// 1/3 JavaBasics
+        System.out.println(fullName);
+
+// 2/3 JavaBasics
+        System.out.println(createFullName("Gabriel", "Sherman"));
+
+    }
+
+// 2/3 JavaBasics
+    public static String createFullName(String First, String Last) {
+        String fullName = First + " " + Last;
+        return fullName;
     }
 }
+
